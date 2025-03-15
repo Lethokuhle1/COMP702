5d6c31c96bf3fdc2f4ccf68e61833ba16ba88801
Lethokuhle <218035115@stu.ukzn.ac.za>
2025-03-15 17:46:56 +0200

    Started with Tut2

---
+++ Tutorial_2/exercise2.py
@@ -0,0 +1,10 @@
+import numpy as np
+import cv2
+
+image = cv2.array([3,7,6,2,0],
+                  [2,4,6,1,1],
+                  [4,7,2,5,4],
+                  [3,0,6,2,1],
+                  [5,7,5,1,2])
+
+portion = (x-1)(x+2), (y-1)(y+2)
